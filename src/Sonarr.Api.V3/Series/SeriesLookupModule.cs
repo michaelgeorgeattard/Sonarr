76aa09328d3a5440c8156dc884fc8c9180b55c04
Mark McDowall <markus.mcd5@gmail.com>
2017-02-10 22:46:39 -0800

    v3 API

---
+++ src/Sonarr.Api.V3/Series/SeriesLookupModule.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using NzbDrone.Core.MediaCover;
+using NzbDrone.Core.MetadataSource;
+using NzbDrone.Core.Tv;
+using Sonarr.Http;
+using Sonarr.Http.Extensions;
+
+namespace Sonarr.Api.V3.Series
+{
+    public class SeriesLookupModule : SonarrRestModule<SeriesResource>
+    {
+        private readonly ISearchForNewSeries _searchProxy;
+        private readonly ISeriesService _seriesService;
+
+        public SeriesLookupModule(ISearchForNewSeries searchProxy, ISeriesService seriesService)
+            : base("/series/lookup")
+        {
+            _searchProxy = searchProxy;
+            _seriesService = seriesService;
+            Get["/"] = x => Search();
+        }
+
+
+        private Response Search()
+        {
+            var tvDbResults = _searchProxy.SearchForNewSeries((string)Request.Query.term);
+            return MapToResource(tvDbResults).AsResponse();
+        }
+
+
+        private static IEnumerable<SeriesResource> MapToResource(IEnumerable<NzbDrone.Core.Tv.Series> series)
+        {
+            foreach (var currentSeries in series)
+            {
+                var resource = currentSeries.ToResource();
+                var poster = currentSeries.Images.FirstOrDefault(c => c.CoverType == MediaCoverTypes.Poster);
+                if (poster != null)
+                {
+                    resource.RemotePoster = poster.Url;
+                }
+
+                yield return resource;
+            }
+        }
+    }
+}
